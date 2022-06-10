4b46dda89d5cbc9857ac9e34507900b873cf26dc
hiyoko3 <16721102+hiyoko3@users.noreply.github.com>
2022-06-11 00:10:22 +0900

    refactor: fix some bugs

--- year/2022/summer/main.js
+++ year/2022/summer/main.js
@@ -1,48 +1,41 @@
-// mp3ファイルの音声を再生
-class Sound {
-  audio;
-
-  constructor() {
-    this.audio = new Audio();
-  }
-
-  play(filename) {
-    this.audio.src = filename;
-    this.audio.play();
-  }
-}
-
 document.addEventListener('DOMContentLoaded', async () => {
+  const GOZOKU_TEXT = '2022年夏の豪族回、今年も開幕決定!';
+  const SE = './mp3/se.mp3';
   const BGM = './mp3/bgm.mp3';
-  const KASYA = './mp3/kasya.mp3';
-  const GOZOKU_TEXT = '2022年夏の豪族回、今年も開幕決定';
+  const audio = new Audio();
 
   // 200ミリ秒待つ
   const util = {
-    sleep: () => {
-      return new Promise((resolve) => setTimeout(resolve, 200));
+    sleep: (timeout = 200) => {
+      return new Promise((resolve) => setTimeout(resolve, timeout));
+    },
+    playSound: async (filename) => {
+      audio.src = filename;
+      await audio.play();
     }
   };
 
-  const sound = new Sound();
+  const gozokuNote = document.getElementById('gozoku-note');
+  const gozokuBtn = document.getElementById('gozoku-btn');
   const kingText = document.getElementById('king-text');
 
   await util.sleep();
 
-  kingText.classList.add('single-word');
+  gozokuBtn.addEventListener('click', async () => {
+    gozokuNote.classList.add('hidden');
+    gozokuBtn.classList.add('hidden');
+    kingText.classList.add('single-word');
 
-  // 音とともに一文字ずつ表示
-  for (let i = 0; i < GOZOKU_TEXT.length; i++) {
-    kingText.innerHTML = '';
-    kingText.innerHTML = GOZOKU_TEXT[i];
-    sound.play(KASYA);
-
-    // 200ミリ秒待つ
-    await util.sleep();
-  }
-  // 全文表示
-  kingText.innerHTML = GOZOKU_TEXT;
-  kingText.classList.remove('single-word');
-  kingText.classList.add('all-words');
-  sound.play(BGM);
+    // 音とともに一文字ずつ表示
+    for (let i = 0; i < GOZOKU_TEXT.length; i++) {
+      kingText.innerHTML = GOZOKU_TEXT[i];
+      await util.playSound(SE);
+      await util.sleep(200);
+    }
+    // 全文表示
+    kingText.innerHTML = GOZOKU_TEXT;
+    kingText.classList.remove('single-word');
+    kingText.classList.add('all-words');
+    await util.playSound(BGM);
+  });
 });
