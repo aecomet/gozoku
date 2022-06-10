eea91f0c48d3703e49331b9505601608021f6741
hiyoko3 <16721102+hiyoko3@users.noreply.github.com>
2022-06-11 00:59:44 +0900

    refactor: use audio context.

--- year/2022/summer/main.js
+++ year/2022/summer/main.js
@@ -1,29 +1,54 @@
-// let isStart = false;
-
-// if ('connection' in navigator) {
-//   if (navigator.connection.type != 'cellular') {
-//     isStart = true;
-//   }
-// }
 document.addEventListener('DOMContentLoaded', async () => {
   const GOZOKU_TEXT = '2022年夏の豪族回、今年も開幕決定!';
+  // audio
   const SE = './mp3/se.mp3';
   const BGM = './mp3/bgm.mp3';
-  const audio = new Audio();
 
-  // if (!isStart) {
-  //   alert('Wifi環境下でページを開いてください。');
-  //   return;
-  // }
+  const audio = {
+    bgm: {
+      context: new AudioContext(),
+      buffer: null,
+      bufferNode: null,
+      init: async () => {
+        const bgmResponse = await fetch(BGM);
+        const bgmArrayBuffer = await bgmResponse.arrayBuffer();
+        audio.bgm.buffer = await audio.bgm.context.decodeAudioData(bgmArrayBuffer);
+        await audio.bgm.prepare();
+      },
+      prepare: async () => {
+        audio.bgm.bufferNode = await audio.bgm.context.createBufferSource();
+        audio.bgm.bufferNode.buffer = audio.bgm.buffer;
+        audio.bgm.bufferNode.connect(audio.bgm.context.destination);
+      }
+    },
+    se: {
+      context: new AudioContext(),
+      buffer: null,
+      bufferNode: null,
+      init: async () => {
+        const seResponse = await fetch(SE);
+        const seArrayBuffer = await seResponse.arrayBuffer();
+        audio.se.buffer = await audio.se.context.decodeAudioData(seArrayBuffer);
+        await audio.se.prepare();
+      },
+      prepare: async () => {
+        audio.se.bufferNode = await audio.se.context.createBufferSource();
+        audio.se.bufferNode.buffer = audio.se.buffer;
+        audio.se.bufferNode.connect(audio.se.context.destination);
+      }
+    }
+  };
 
-  // 200ミリ秒待つ
+  await audio.bgm.init();
+  await audio.se.init();
+
   const util = {
     sleep: (timeout = 200) => {
       return new Promise((resolve) => setTimeout(resolve, timeout));
     },
-    playSound: async (filename) => {
-      audio.src = filename;
-      await audio.play();
+    playSound: async (key) => {
+      audio[key].bufferNode.start(0);
+      audio[key].prepare();
     },
     isMobile: () => {
       // デバイス幅が640px以下の場合にスマホと判定する
@@ -36,8 +61,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   const kingText = document.getElementById('king-text');
   const isMobile = util.isMobile();
 
-  await util.sleep();
-
   gozokuBtn.addEventListener('click', async () => {
     gozokuNote.classList.add('hidden');
     gozokuBtn.classList.add('hidden');
@@ -46,7 +69,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // 音とともに一文字ずつ表示
     for (let i = 0; i < GOZOKU_TEXT.length; i++) {
       kingText.innerHTML = GOZOKU_TEXT[i];
-      await util.playSound(SE);
+      await util.playSound('se');
       if (!isMobile) {
         await util.sleep(210);
       }
@@ -55,6 +78,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     kingText.innerHTML = GOZOKU_TEXT;
     kingText.classList.remove('single-word');
     kingText.classList.add('all-words');
-    await util.playSound(BGM);
+    await util.playSound('bgm');
   });
 });
