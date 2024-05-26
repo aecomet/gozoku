393303841a53e31c6f2ce6ac900093bcba04e744
aecomet <16721102+aecomet@users.noreply.github.com>
2024-05-26 14:20:59 +0900

    feat: add base template

---
+++ vite.config.mjs
@@ -0,0 +1,40 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+// https://vitejs.dev/config/
+export default () => {
+  const SRC_PATH = 'src';
+  const OUTPUT_PATH = 'build';
+
+  return defineConfig({
+    root: path.resolve(__dirname, SRC_PATH),
+    resolve: {
+      alias: [
+        {
+          find: '@src',
+          replacement: path.resolve(__dirname, SRC_PATH)
+        }
+      ],
+      extensions: ['.ts']
+    },
+    plugins: [
+      // generate html
+      createHtmlPlugin(),
+      // lint
+      eslintPlugin({
+        fix: true
+      })
+    ],
+    publicDir: path.resolve(__dirname, `${SRC_PATH}/assets`),
+    build: {
+      outDir: path.resolve(__dirname, OUTPUT_PATH),
+      emptyOutDir: true
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8888
+    }
+  });
+};
