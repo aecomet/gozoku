67f69d0fd4e3d37e9eea2d42000b0f49d31e295c
aecomet <16721102+aecomet@users.noreply.github.com>
2024-05-27 10:07:58 +0900

    feat: Add schedule page

---
+++ src/app/schedule/Schedule.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule',
+  standalone: true,
+  templateUrl: './Schedule.component.html',
+  styleUrl: './Schedule.component.css'
+})
+export class AppSchedule {}
